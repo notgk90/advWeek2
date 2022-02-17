971f44dedbd3d598813cf3e9bf0661aca012dbcb
Bayu <s160419164.student.ubaya.ac.id>
2022-02-17 12:44:25 +0700

    init

---
+++ app/src/main/java/com/ubaya/adv160419108week2/GameFragment.kt
@@ -0,0 +1,35 @@
+package com.ubaya.adv160419108week2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_game.*
+import kotlinx.android.synthetic.main.fragment_main.*
+import kotlinx.android.synthetic.main.fragment_main.btnStart
+
+class GameFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_game, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if(arguments != null){
+            val playerName = GameFragmentArgs.fromBundle(requireArguments()).playerName
+            txtTurn.text = "$playerName's Turn"
+        }
+
+        btnBack.setOnClickListener {
+            val action = GameFragmentDirections.actionMainFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+}
