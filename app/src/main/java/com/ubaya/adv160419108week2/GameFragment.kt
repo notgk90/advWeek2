52da39f3c88a78bfaf56681965ba6d77f184a773
MSI-NOTEBOOK <gilangkusuma90@gmail.com>
2022-02-23 13:47:22 +0800

    init

--- app/src/main/java/com/ubaya/adv160419108week2/GameFragment.kt
+++ app/src/main/java/com/ubaya/adv160419108week2/GameFragment.kt
@@ -7,8 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_game.*
-import kotlinx.android.synthetic.main.fragment_main.*
-import kotlinx.android.synthetic.main.fragment_main.btnStart
 
 class GameFragment : Fragment() {
 
@@ -26,10 +24,29 @@ class GameFragment : Fragment() {
             val playerName = GameFragmentArgs.fromBundle(requireArguments()).playerName
             txtTurn.text = "$playerName's Turn"
         }
+        var score = 0
+        var num1 =(0..100).random()
+        var num2 =(0..100).random()
+        var res = 0
 
-        btnBack.setOnClickListener {
-            val action = GameFragmentDirections.actionMainFragment()
-            Navigation.findNavController(it).navigate(action)
+        res = num1 + num2
+        txtNum1.setText(num1.toString())
+        txtNum2.setText(num2.toString())
+        btnSubmit.setOnClickListener {
+            var answer = txtAnswer.text.toString()
+            if (answer == res.toString()) {
+                var A = (0..100).random()
+                var B = (0..100).random()
+                res = A + B
+                txtNum1.setText(A.toString())
+                txtNum2.setText(B.toString())
+                score += 1
+                txtAnswer.setText("")
+            } else {
+                val totalscore = score.toString()
+                val action = GameFragmentDirections.actionResultFragment(totalscore)
+                Navigation.findNavController(it).navigate(action)
+            }
         }
     }
 }
